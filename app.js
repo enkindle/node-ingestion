278107ce3893e07028282d8345f6a5663446f5a5
James Hansen <enkindledesign@gmail.com>
2017-05-01 16:11:11 -0600

    XML Parsing Function
    First stab at the xml parsing function. OOA specific configuration.

--- app.js
+++ app.js
@@ -1,24 +1,66 @@
 'use strict';
 // Configuration for folder watch
 // TODO - This needs to be moved out into a configuration file
-const config = {
-	watchFolder : 'uploads',
-	fileType : 'pdf'
-};
+// const config = {
+// 	watchFolder : 'uploads',
+// 	fileType : 'pdf'
+// };
+
+// OOA specific configuration
+const ooaConfig = {
+	watchFolder : 'ooa-ingestion',
+	fileType : 'XML'
+}
 
 // Set the required variable for the app
 const logger = require('./components/logger.js').logger;
 const chokidar = require('chokidar');
 const path = require('path');
-const folderPath = './' + config.watchFolder + "/*" + config.fileType;
-
+const folderPath = './' + ooaConfig.watchFolder + "/*" + ooaConfig.fileType;
+const fs = require('fs');
+const fileFolder = './' + ooaConfig.watchFolder + '/';
+const xmlParser = require('xml2js').parseString;
+logger.info('app started');
+logger.info(fileFolder);
 
 // Use chokidar to watch the uploads folder for incoming files
-chokidar.watch(folderPath, {ignored: /[\/\\]\./}).on('all', (event, filePath) => {
-  let fileName = path.basename(filePath);
-  parseXML(fileName);
+const ooaWatcher = chokidar.watch(folderPath, {
+	ignored: /[\/\\]\./,
+	ignoreInitial: true
 });
 
-function parseXML(fileName){
-	logger.info(fileName);
+ooaWatcher.on('add', function(filePath){
+	let fileName = path.basename(filePath);
+	logger.info('add function');
+	if (ooaConfig.fileType === 'XML'){
+		parseXML(filePath);
+	}
+	//else if (ooaConfig.fileType === 'pdf'){
+
+	// }
+});
+
+function parseXML(filePath){
+	logger.info('parsing xml');
+	let fileData = null
+	fs.readFile(filePath, 'utf8', function(err, data){
+		if (err){
+			return logger.error(err);
+		}
+
+		logger.info(data);
+
+		xmlParser(data + '', function(err, result){
+			//logger.info(result);
+			fileData = result.Batch;
+		});
+		console.log(typeof fileData);
+		logger.info(fileData.Documents[0].Document);
+		let documents = fileData.Documents[0].Document;
+		Object.keys(documents).map(function(objectKey, index){
+			//logger.info(documents[objectKey]);
+			logger.info(documents[objectKey].FileName[0]);
+		});
+	});
+	//logger.info(fileName);
 }
